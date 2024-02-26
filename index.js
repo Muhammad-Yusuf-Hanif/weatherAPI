b5eb8e47bb42eda5fe6ddd2f6f8544dbd3d54fa8
Muhammad-Yusuf-Hanif <yusufhanif786@hotmail.co.uk>
2024-02-26 00:10:03 +0000

    completed API calls and styling for mobile responsiveness

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-const inputText = document.getElementById("inputText");
 const searchBar = document.getElementById("searchBar");
+const inputText = document.getElementById("inputText");
+const weatherDisplay = document.querySelector(".weatherDisplay");
+const weatherIcon = document.getElementById("weatherIcon");
 const temp = document.getElementById("temp");
 const locationName = document.getElementById("location");
+const humidity = document.getElementById("humidity");
+const humidityIcon = document.getElementById("humidityIcon");
+const windSpeed = document.getElementById("windSpeed");
+const windSpeedIcon = document.getElementById("windSpeedIcon");
+
 const APIKey = "96ae4f0ff15d3b006deaf259d7638b26";
 const APIUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-inputText.addEventListener("focusin", function inputTextClick(e) {
+inputText.addEventListener("focusin", (e) => {
 	e.target.placeholder = "";
 });
 
-inputText.addEventListener("focusout", function inputTextClick(e) {
+inputText.addEventListener("focusout", (e) => {
 	e.target.placeholder = "Search Country/City Here";
 });
 
 function getWeather(data) {
-  const locationWeather = data.weather[0].main;
-  const locationTemp = Math.round(data.main.temp);
-  const locationRegion = data.name;
-  const locationHumidity = data.main.humidity + "%";
-  const locationWindSpeed = data.wind.speed + " M/Ps";
-  temp.innerHTML = locationTemp + ` &deg;C`;
-  locationName.innerHTML = locationRegion;
+	const locationWeather = data.weather[0].main;
+	const locationTemp = Math.round(data.main.temp);
+	const locationRegion = data.name;
+	const locationHumidity = data.main.humidity;
+	const locationWindSpeed = data.wind.speed + " M/Ps";
+
+	console.log("locationHumidity", locationHumidity);
+
+	temp.innerHTML = locationTemp + ` &deg;C`;
+	locationName.innerHTML = locationRegion;
+	humidity.innerHTML = locationHumidity + "%";
+	windSpeed.innerHTML = locationWindSpeed;
+
+	if (locationWeather == "Clouds") {
+		weatherIcon.src = "assets/weather_icons/cloud/cloudy.png";
+	} else if (locationWeather == "Clear") {
+		weatherIcon.src = "assets/weather_icons/sun/sunny&cloudy.png";
+	} else if (locationWeather == "Rain") {
+		weatherIcon.src = "assets/weather_icons/rain/39.png";
+	} else if (locationWeather == "Drizzle") {
+		weatherIcon.src = "assets/weather_icons/sun/sunny&raining.png";
+	} else if (locationWeather == "Mist") {
+		weatherIcon.src = "assets/weather_icons/sun/sunny.png";
+	} 
+
+	weatherDisplay.style.display = "block";
+
+	humidityIcon.src = "assets/Humidity.webp";
+	windSpeedIcon.src = "assets/WindSpeed.png";
 }
 
 searchBar.addEventListener("submit", function (e) {
 	e.preventDefault();
-	const locationValue = inputText.value;
+	const locationValue = inputText.value.trim();
 	const fullAPIUrl =
 		APIUrl + locationValue + "&appid=" + APIKey + "&units=metric";
 
